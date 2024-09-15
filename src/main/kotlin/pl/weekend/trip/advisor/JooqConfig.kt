6a3e98e10c5d510ac998cdbec7dc2e08e11a2c43
felalag <iraboboyed@gmail.com>
2024-09-15 18:23:40 +0200

    Fix configuration classes

--- src/main/kotlin/pl/weekend/trip/advisor/JooqConfig.kt
+++ src/main/kotlin/pl/weekend/trip/advisor/JooqConfig.kt
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.r2dbc.connection.TransactionAwareConnectionFactoryProxy
 
 @Configuration
-class JooqConfig
-
-@Bean
-fun dslContext(connectionFactory: ConnectionFactory) =
-  DSL.using(TransactionAwareConnectionFactoryProxy(connectionFactory), SQLDialect.MYSQL)
+class JooqConfig {
+  @Bean
+  fun dslContext(connectionFactory: ConnectionFactory) =
+    DSL.using(TransactionAwareConnectionFactoryProxy(connectionFactory), SQLDialect.MYSQL)
+}
--- src/main/kotlin/pl/weekend/trip/advisor/SettingRouter.kt
+++ src/main/kotlin/pl/weekend/trip/advisor/SettingRouter.kt
@@ -6,14 +6,14 @@ import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
-class SettingRouter
-
-@Bean
-fun route(settingHandler: SettingHandler) = coRouter {
-  accept(APPLICATION_JSON).nest {
-    GET("/settings/{id}", settingHandler::getSetting)
-    POST("/settings", settingHandler::createSetting)
-    PUT("/settings/{id}", settingHandler::updateSetting)
-    DELETE("/settings/{id}", settingHandler::deleteSetting)
+class SettingRouter {
+  @Bean
+  fun route(settingHandler: SettingHandler) = coRouter {
+    accept(APPLICATION_JSON).nest {
+      GET("/settings/{id}", settingHandler::getSetting)
+      POST("/settings", settingHandler::createSetting)
+      PUT("/settings/{id}", settingHandler::updateSetting)
+      DELETE("/settings/{id}", settingHandler::deleteSetting)
+    }
   }
 }

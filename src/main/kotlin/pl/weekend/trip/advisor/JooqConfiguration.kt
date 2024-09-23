c658e8c50008255356d821316d51c06c38bb4788
felalag <iraboboyed@gmail.com>
2024-09-23 18:32:48 +0200

    Add dsl context

---
+++ src/main/kotlin/pl/weekend/trip/advisor/JooqConfiguration.kt
@@ -0,0 +1,14 @@
+package pl.weekend.trip.advisor
+
+import io.r2dbc.spi.ConnectionFactory
+import org.jooq.SQLDialect
+import org.jooq.impl.DSL
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.r2dbc.connection.TransactionAwareConnectionFactoryProxy
+
+@Configuration
+class JooqConfiguration {
+  @Bean
+  fun dslContext(connectionFactory: ConnectionFactory) = DSL.using(TransactionAwareConnectionFactoryProxy(connectionFactory), SQLDialect.MYSQL)
+}

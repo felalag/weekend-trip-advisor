6f0f8df82acf88b3efdc1edfac456b176c6518d8
felalag <iraboboyed@gmail.com>
2024-12-01 16:51:46 +0100

    Add queries for user table

--- src/main/kotlin/pl/weekend/trip/advisor/users/UserRepository.kt
+++ src/main/kotlin/pl/weekend/trip/advisor/users/UserRepository.kt
@@ -1,24 +1,32 @@
 package pl.weekend.trip.advisor.users
 
 import org.jooq.DSLContext
+import org.jooq.generated.Tables.USERS
+import org.jooq.generated.tables.records.UsersRecord
 import org.springframework.stereotype.Repository
 
 @Repository
 class UserRepository(private val dslContext: DSLContext) {
 
-  fun getUserById(id: Long): User? {
-    TODO()
-  }
+  fun getUserById(id: Long): UsersRecord? =
+    dslContext.selectFrom(USERS).where(USERS.ID.eq(id)).fetchOne()
 
-  fun createUser(user: User): User {
-    TODO()
-  }
+  fun createUser(user: User) =
+    dslContext
+      .insertInto(USERS)
+      .columns(USERS.NAME, USERS.PASSPORT_CODE)
+      .values(user.name, user.passportCode)
+      .execute()
 
-  fun updateUser(user: User): User {
-    TODO()
-  }
+  fun updateUser(user: User) =
+    dslContext.update(USERS)
+      .set(USERS.NAME, user.name)
+      .set(USERS.PASSPORT_CODE, user.passportCode)
+      .where(USERS.ID.eq(user.id))
+      .execute()
 
-  fun deleteUser(user: User): User {
-    TODO()
-  }
+  fun deleteUser(user: User) =
+    dslContext.deleteFrom(USERS)
+      .where(USERS.ID.eq(user.id))
+      .execute()
 }
